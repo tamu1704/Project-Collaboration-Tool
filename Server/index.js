c8df3ffdc7b615a568b748751ea9e98bf196f8f9
Tamanna <tamanna71704@gmail.com>
2025-08-15 22:00:56 +0530

    more files added

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db'); 
-
-dotenv.config(); 
-
-const app = express();
-
-//Put middlewares here
-app.use(cors());
-app.use(express.json());
-
-connectDB();
-
-//Put routes here
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("disconnect", () => console.log("A user disconnected"));
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
