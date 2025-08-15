d35693c7ece9ba0703082fb659244a0f9fcc75f3
Tamanna <tamanna71704@gmail.com>
2025-08-15 21:50:19 +0530

    initial files added

---
+++ Server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
+
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    role:{type:String,enum:["Admin","ProjectManager","TeamMember"],default:"TeamMember"}
+},{timestamps:true});
+
+userSchema.pre("save",async function(next){
+    if(!this.modifiedPaths("password")) return next();
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
